f88e33dd424450f7748471279d22f7bccc324e3c
mscherer <mark.scherer@spryker.com>
2020-03-18 00:15:49 +0100

    Disable hooks.

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,4 +5,15 @@ namespace Data;
 use Cake\Core\BasePlugin;
 
 class Plugin extends BasePlugin {
+
+	/**
+	 * @var bool
+	 */
+	protected $middlewareEnabled = false;
+
+	/**
+	 * @var bool
+	 */
+	protected $consoleEnabled = false;
+
 }
