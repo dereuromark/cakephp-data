653d6ab8a98705dd1c76df01e1685f1eaed92e01
mscherer <mark.scherer@spryker.com>
2020-10-19 22:18:57 +0200

    make actions private.

--- src/Controller/DataAppController.php
+++ src/Controller/DataAppController.php
@@ -3,7 +3,6 @@
 namespace Data\Controller;
 
 use App\Controller\AppController;
-use Cake\Event\EventInterface;
 
 /**
  * @property \Tools\Controller\Component\CommonComponent $Common
@@ -19,18 +18,6 @@ class DataAppController extends AppController {
 		$this->loadComponent('Tools.Common');
 
 		$this->viewBuilder()->setHelpers(['Tools.Common', 'Tools.Format', 'Tools.Time', 'Tools.Number', 'Tools.Text', 'Data.Data']);
-	}
-
-	/**
-	 * @param \Cake\Event\EventInterface $event
-	 * @return \Cake\Http\Response|null|void
-	 */
-	public function beforeFilter(EventInterface $event) {
-		parent::beforeFilter($event);
-
-		if (isset($this->Auth)) {
-			$this->Auth->allow();
-		}
 	}
 
 }
