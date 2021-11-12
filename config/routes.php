ca4acfdc8eb8ae730375867ddb86838994f92548
mscherer <mark.scherer@spryker.com>
2021-11-12 11:26:00 +0100

    Fix up routes file.

--- config/routes.php
+++ config/routes.php
@@ -3,20 +3,18 @@
  * @var \Cake\Routing\RouteBuilder $routes
  */
 
-use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
 $routes->prefix('Admin', function (RouteBuilder $routes) {
 		$routes->plugin('Data', function (RouteBuilder $routes) {
-			$routes->connect('/', ['action' => 'index'], ['routeClass' => DashedRoute::class]);
+			$routes->connect('/', ['action' => 'index']);
 
-			$routes->fallbacks(DashedRoute::class);
+			$routes->fallbacks();
 		});
 });
 
 $routes->plugin('Data', function (RouteBuilder $routes) {
-	$routes->connect('/', ['action' => 'index'], ['routeClass' => DashedRoute::class]);
+	$routes->connect('/', ['action' => 'index']);
 
-	$routes->fallbacks(DashedRoute::class);
+	$routes->fallbacks();
 });
