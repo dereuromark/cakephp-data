0a6c150334dff7d2085df2d253fbee0907f14c7b
mscherer <dereuromark@web.de>
2023-03-22 18:31:24 +0100

    Make more unobstrusive

--- src/Controller/Component/CountryStateHelperComponent.php
+++ src/Controller/Component/CountryStateHelperComponent.php
@@ -28,9 +28,10 @@ class CountryStateHelperComponent extends Component {
 	 * @param bool $ignoreStates
 	 * @param string|null $prefix Data prefix if applicable (e.g. user_info.country_id)
 	 * @param int $defaultValue
+	 * @param int|null $selectedCountry
 	 * @return void
 	 */
-	public function provideData($ignoreStates = false, $prefix = null, $defaultValue = 0) {
+	public function provideData($ignoreStates = false, $prefix = null, $defaultValue = 0, $selectedCountry = null) {
 		$this->Controller->loadModel('Data.Countries');
 		$countries = $this->Controller->Countries->findActive()->enableHydration(false)->find('list')->toArray();
 
@@ -44,9 +45,10 @@ class CountryStateHelperComponent extends Component {
 			$this->Controller->loadModel('Data.States');
 		}
 
-		$selectedCountry = $this->Controller->getRequest()->getQuery($field);
 		if ($this->Controller->getRequest()->getData($field)) {
 			$selectedCountry = $this->Controller->getRequest()->getData($field);
+		} elseif ($this->Controller->getRequest()->getQuery($field)) {
+			$selectedCountry = $this->Controller->getRequest()->getQuery($field);
 		}
 
 		if ($selectedCountry) {
