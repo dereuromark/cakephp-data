5ca244833ed1c79c6498323f90e1bec658850ff7
mscherer <dereuromark@web.de>
2023-11-13 12:12:54 +0100

    Fix tests for mysql

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -92,6 +92,9 @@ ConnectionManager::setConfig('test', [
 	'timezone' => 'UTC',
 	'quoteIdentifiers' => true,
 	'cacheMetadata' => true,
+	'flags' => [
+		PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))",
+	],
 ]);
 
 Configure::write('Icon', [
